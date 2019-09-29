610f228960260b1aba3bf7df9a158e344df012c0
kelvim saidel <Kelvim>
2019-09-29 20:06:59 -0300

    Classe Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import modelo.entidade.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "books");
+		System.out.println(obj);
+	}
+
+}
